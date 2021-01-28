cede5ee68f5127304bcbf6bc3c0df9502a925fde
Ming Li <mingfinkli@gmail.com>
2021-01-28 04:00:22 +0000

    return pointer

--- c_bfs.c
+++ c_bfs.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 //#include "bfs.h"
 /*
 typedef struct{
@@ -18,7 +19,10 @@ int bfs1(int *a, int *b, int *c, int *d, int *e, int *f, int g, int m, int n, in
     return 2;
 }
 
-int bfs(int *topo, int *num_ex, int *ex_names, int *pre_priv, int *post_priv, int *pacc, int cont_cnt, int outside_id, int docker_host_id, int max_num_ex, int *node_name, int *node_priv, int *edge_start, int *edge_end, int *edge_label, int *node_cnt, int *edge_cnt){
-    printf("+++++ print from C +++++ %d %d %d %d %d %d\n", cont_cnt, outside_id, docker_host_id, max_num_ex, *node_cnt, *edge_cnt); 
-    return 3;
+int *bfs(int *topo, int *num_ex, int *ex_names, int *pre_priv, int *post_priv, int *pacc, int cont_cnt, int outside_id, int docker_host_id, int max_num_ex, int *node_name, int *node_priv, int *edge_start, int *edge_end, int *node_cnt, int *edge_cnt){
+    printf("+++++ print from C +++++ %d %d %d %d %d %d\n", cont_cnt, outside_id, docker_host_id, max_num_ex, *node_cnt, *edge_cnt);
+    int *edge_label = (int *)malloc(sizeof(int)*2500000*100);
+    edge_label[2500000*100-1]=35;
+    edge_label[0]=53;  
+    return edge_label;
 }
