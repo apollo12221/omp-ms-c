8ec68bdfc409bef79d131160a24b7c349c52b5d9
Ming Li <mingfinkli@gmail.com>
2021-01-28 01:42:04 +0000

    initial

---
+++ c_bfs.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int bfs(int *a, int *b, int *c, int *d, int *e, int *f, int g, int m, int n, int p){
+    printf("+++++ print from C +++++ %d %d %d %d\n", g, m, n, p);
+    return 2;
+}
