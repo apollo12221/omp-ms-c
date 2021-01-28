f9eccb9ae47faf76fe78be0e6978b06696ef944b
Ming Li <mingfinkli@gmail.com>
2021-01-28 03:29:22 +0000

    slow

--- c_bfs.c
+++ c_bfs.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+//#include "bfs.h"
+/*
+typedef struct{
+    int numOfNodes;
+    int nodeName[100000];
+    int nodePriv[100000];
+    int node_hash[100000];
+    int numOfEdges;
+    int edge_start_name[8000000];
+    int edge_start_
+}*/
 
-int bfs(int *a, int *b, int *c, int *d, int *e, int *f, int g, int m, int n, int p){
+
+
+int bfs1(int *a, int *b, int *c, int *d, int *e, int *f, int g, int m, int n, int p){
     printf("+++++ print from C +++++ %d %d %d %d\n", g, m, n, p);
     return 2;
+}
+
+int bfs(int *topo, int *num_ex, int *ex_names, int *pre_priv, int *post_priv, int *pacc, int cont_cnt, int outside_id, int docker_host_id, int max_num_ex, int *node_name, int *node_priv, int *edge_start, int *edge_end, int *edge_label, int *node_cnt, int *edge_cnt){
+    printf("+++++ print from C +++++ %d %d %d %d %d %d\n", cont_cnt, outside_id, docker_host_id, max_num_ex, *node_cnt, *edge_cnt); 
+    return 3;
 }
